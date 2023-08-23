784d904f4cdcc656f51eae14e27b809387c20639
fancn21th <fancn21th@gmail.com>
2023-08-23 17:15:34 +0800

    📦 NEW: gridster

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,11 +1,12 @@
 <script setup>
 import Layer from "../../components/layer/Layer.vue";
+import Grid from "./components/grid/Grid.vue";
 </script>
 
 <template>
   <!-- gridster layer -->
   <Layer :index="100">
-    <h1>Gridster</h1>
+    <Grid />
   </Layer>
   <!-- map layer -->
   <Layer :index="10">
