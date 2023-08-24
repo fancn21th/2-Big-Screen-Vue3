5770dd20fb45b137346b8a221e44ea50e3582e99
fancn21th <fancn21th@gmail.com>
2023-08-24 17:18:43 +0800

    📦 NEW: 4 layers layout

--- src/layouts/basicLayout/BasicLayout.vue
+++ src/layouts/basicLayout/BasicLayout.vue
@@ -1,28 +1,19 @@
 <script setup>
 import Layer from "../../components/layer/Layer.vue";
+import Nav from "./components/nav/Nav.vue";
 </script>
 
 <template>
   <!-- nav layer -->
   <Layer :index="1000">
-    <div class="nav">Navigation Layer</div>
+    <Nav />
     <router-view></router-view>
   </Layer>
   <!-- background layer -->
-  <Layer :index="1">
-    <div class="background">Background</div>
-  </Layer>
+  <Layer :index="1"> </Layer>
 
   <portal-target name="destination" multiple />
 </template>
 
 <style scoped>
-.nav {
-  width: 100%;
-  height: 100%;
-}
-.background {
-  width: 100%;
-  height: 100%;
-}
 </style>
---
+++ src/layouts/basicLayout/components/nav/Nav.vue
@@ -0,0 +1,32 @@
+<script setup>
+</script>
+
+<template>
+  <div class="container">
+    <div class="header bg">Navigation Layer / Header</div>
+    <div class="nav bg">Navigation Layer / Left Navbar</div>
+  </div>
+</template>
+
+<style scoped>
+* {
+}
+.bg {
+  border: 1px dashed #ccc;
+  background-color: #eee;
+}
+.header {
+  width: 500px;
+  height: 100px;
+}
+.nav {
+  width: 100px;
+  height: 500px;
+}
+.container {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+}
+</style>
