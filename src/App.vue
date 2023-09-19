d845753c2ac324bf9b8aa153c026f6a7bbe996a3
fancn21th <fancn21th@gmail.com>
2023-09-19 21:16:02 +0800

    👌 IMPROVE: fixed overall width and height

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,23 @@
-<script setup></script>
+<script setup>
+import { onMounted } from "vue";
+import config from "@/configs/layerConfig";
+const { designWidthPx, designHeightPx } = config;
+
+onMounted(() => {
+  // refer to https://stackoverflow.com/questions/76090183/using-v-bind-to-set-css-vars-in-vue-3-composition-issue
+  document.documentElement.style.setProperty("--design-width", designWidthPx);
+  document.documentElement.style.setProperty("--design-height", designHeightPx);
+});
+</script>
 
 <template>
   <router-view></router-view>
 </template>
 
-<style></style>
+<style>
+#app {
+  position: relative;
+  width: var(--design-width);
+  height: var(--design-height);
+}
+</style>
