543328b7d52dd9850b07e2dd61894621ebcbbaf0
fancn21th <fancn21th@gmail.com>
2023-09-29 07:17:21 +0800

    📦 NEW: 大屏自动伸缩 scale

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,20 @@
 <script setup>
-import { onMounted } from "vue";
+import { onBeforeMount } from "vue";
 import config from "@/configs/layerConfig";
-const { designWidthPx, designHeightPx } = config;
+const { designWidthPx, designHeightPx, designWidth, designHeight } = config;
 
-onMounted(() => {
+onBeforeMount(() => {
+  const scale =
+    document.documentElement.clientWidth /
+      document.documentElement.clientHeight <
+    designWidth / designHeight
+      ? document.documentElement.clientWidth / designWidth
+      : document.documentElement.clientHeight / designHeight;
+
   // refer to https://stackoverflow.com/questions/76090183/using-v-bind-to-set-css-vars-in-vue-3-composition-issue
   document.documentElement.style.setProperty("--design-width", designWidthPx);
   document.documentElement.style.setProperty("--design-height", designHeightPx);
+  document.documentElement.style.setProperty("--scale", scale);
 });
 </script>
 
@@ -19,5 +27,7 @@ onMounted(() => {
   position: relative;
   width: var(--design-width);
   height: var(--design-height);
+  transform: scale(var(--scale));
+  transform-origin: left top;
 }
 </style>
